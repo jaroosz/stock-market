8fded25ca6646e2535b16ac83cc41fe9cc8033b4
Dawid <jarosz199806@gmail.com>
2026-05-02 14:55:26 +0200

    feat: add express routes for wallets, stocks, and logs with error handling

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import walletsRouter from "./routes/wallets.routes";
+import stocksRouter from "./routes/stocks.routes";
+import logRouter from "./routes/log.routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/wallets', walletsRouter);
+app.use('/stocks', stocksRouter);
+app.use('/log', logRouter);
+
+app.post('/chaos', (req, res) => {
+    res.status(200).send();
+    process.exit(0);
+});
+
+app.use(errorHandler);
 
 export default app
